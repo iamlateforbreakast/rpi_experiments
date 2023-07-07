4c42e24c2b72b044bb02dc2d62d39ec7b316952d
Thomas Remion <thomas.remion@virgin.net>
2023-07-07 14:35:26 +0100

    Load html and css files

--- E01_Create_CherryPy_WebServer/tut01.py
+++ E01_Create_CherryPy_WebServer/tut01.py
@@ -1,11 +1,20 @@
+import os, os.path
+
 import cherrypy
 
 class HelloWorld(object):
 	@cherrypy.expose
 	def index(self):
-		return "Hello world!"
+		return open("index.html")
 
 if __name__ == '__main__':
+	conf= {
+		'/': {
+			'tools.staticdir.root': os.path.abspath(os.getcwd()) },
+		'/static': {
+			'tools.staticdir.on': True,
+			'tools.staticdir.dir': './public' }
+		} 
 	cherrypy.config.update({'server.socket_host': '0.0.0.0'})	
 	cherrypy.config.update({'server.socket_port': 8080})	
-	cherrypy.quickstart(HelloWorld())
+	cherrypy.quickstart(HelloWorld(), '/', conf)
--- Readme.md
+++ Readme.md
@@ -1,5 +1,7 @@
 # A collection of experiments for the Raspberry Pi.
 
+## Setup access to githu from raspberry pi
+
 Create an SSH key for the Raspberry Pi.
 
 `ssh-keygen -t rsa -b 4096 -C "name@email.com"`
@@ -10,4 +12,7 @@ Test
 `ssh -T git@github.com`
 
 `git remote set-url origin git@github:iamlateforbreakast/rpi_experiments.git`
+
+`git config pull.rebase false`
+
 
