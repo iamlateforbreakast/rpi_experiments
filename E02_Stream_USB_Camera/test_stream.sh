1c36fb216f98f05a110026e8948c0fa210d36c4f
Thomas Remion <thomas.remion@virgin.net>
2023-07-16 12:39:29 +0100

    Additional attemps to run webrtc with gstreamer

--- E02_Stream_USB_Camera/test_stream.sh
+++ E02_Stream_USB_Camera/test_stream.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-gst-launch-1.0 videotestsrc ! videoconvert ! tcpserversink host=127.0.0.1 port=8090
+gst-launch-1.0 videotestsrc ! webrtcsink meta="meta,name=gst-stream"
+#gst-launch-1.0 videotestsrc is-live=true ! x264enc ! h264parse ! hlssink2 max-files=5
+#gst-launch-1.0 videotestsrc ! vp8enc ! webmmux name=stream streamable=true ! tcpserversink host=localhost port=8090
+#gst-launch-1.0 videotestsrc ! videoconvert ! video/x-raw,width=320,height=240 ! x264enc key-int-max=12 ! mpegtsmux ! tcpserversink host=localhost port=8090
 #rtspsrc location=rtsp://user:***@192.168.1.5:554 ! rtph264depay !
 #h264parse ! qtmux ! <http compatible sink>
+#        ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
+
 
