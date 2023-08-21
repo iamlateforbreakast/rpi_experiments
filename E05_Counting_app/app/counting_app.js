b461dba5e6107c2fb8e2d3e191e1162ad491cbe3
Thomas Remion <thomas.remion@virgin.net>
2023-08-21 11:02:28 +0100

    Correct after test

--- E05_Counting_app/app/counting_app.js
+++ E05_Counting_app/app/counting_app.js
@@ -4,33 +4,31 @@ const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-   res.sendfile('index.html');
+   res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/raphael-min.js', function(req, res) {
-   res.sendfile('raphael-min.js');
+   res.sendFile(__dirname + "/raphael-min.js");
 });
 
 server.listen(port, function() {
    console.log(`Listening on port ${port}`);
 });
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-})
+//io.on('connection', function(socket) {
+//  console.log('user connected');
+//  socket.on('disconnect', function () {
+//    console.log('user disconnected');
+//  });
+//})
 
-//io.sockets.on('connection', function (socket) {
-    //socket.on('message', function (message) {
-//        console.log("Got connection");
-//        counter = 0;
-//        setInterval(function () {
-//          counter = counter + 1;
-//          console.log("Counting " + counter);
-//          socket.emit('counter', { 'val': counter });
-//        }, 500);
-    //});
-//});
+io.on('connection', function (socket) {
+    console.log("Got connection");
+    counter = 0;
+    setInterval(function () {
+       counter = counter + 1;
+       console.log("Counting " + counter);
+       socket.emit('counter', { 'val': counter });
+        }, 500);
+});
 
